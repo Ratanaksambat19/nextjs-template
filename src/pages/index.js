9f1f361bbd5ced4358ab887cdf049fb9e55ad998
Enigma001 <enigma001@Enigma001s-MacBook-Air.local>
2022-07-12 15:13:11 +0700

    add seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,17 @@ import React from 'react';
 export default function Home() {
   return <div className="text-2xl text-green-600 text-center">Hello world</div>;
 }
+
+export async function getStaticProps() {
+  const homeSeo = {
+    title: 'home page',
+    description: 'hello this is homepage',
+    images: '',
+  };
+  return {
+    props: {
+      seo: homeSeo,
+      linkTo: '/',
+    },
+  };
+}
--- src/util/dev.js
+++ src/util/dev.js
@@ -1,4 +1,3 @@
 export const isDebug = () => process?.env?.NODE_ENV === 'development';
 
-export const getDomain = () =>
-  isDebug() ? 'http://localhost:3000' : 'https://sa-gaming-site.vercel.app';
+export const getDomain = () => (isDebug() ? 'http://localhost:3000' : '');
