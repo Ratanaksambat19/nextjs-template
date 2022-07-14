dc3852ec69d133af71a8cef2d10f994fdddb6f56
Enigma001 <enigma001@Enigma001s-MacBook-Air.local>
2022-07-14 15:41:40 +0700

    add lint staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
     'prettier',
   ],
   parserOptions: {
@@ -17,15 +18,35 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ['react', 'jsx-a11y'],
+  rules: {
+    'jsx-a11y/alt-text': [
+      2,
+      {
+        elements: ['img', 'object', 'area', 'input[type="image"]'],
+        img: ['Image'],
+        object: ['Object'],
+        area: ['Area'],
+        'input[type="image"]': ['InputImage'],
+      },
+    ],
+  },
   settings: {
     react: {
       version: 'latest',
     },
     'import/resolver': {
       node: {
-        paths: ['src'],
+        paths: ['src', 'components'],
+      },
+      alias: {
+        map: [
+          ['@', './src'],
+          ['@components', './src/components'],
+          ['@common', './src/components/Common'],
+          ['@layout', './src/components/Layout'],
+          ['@constants', './src/constants'],
+        ],
       },
     },
   },
